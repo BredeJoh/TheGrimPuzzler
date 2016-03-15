00726f6eca05576aa98ae7f069a71c8b47c3b639
MartinLien <m.lsk95@hotmail.com>
2016-03-15 13:11:00 +0100

    flavour text prefabs

--- Assets/Flavourtext.cs
+++ Assets/Flavourtext.cs
@@ -13,18 +13,30 @@ public class Flavourtext : MonoBehaviour {
 		flavour = new GameObject[3];
 
 		//Finding skeleton flavour text
-		flavour[0] = GameObject.Find ("SkeleFlav");
-		flavour[0].SetActive (false);
+		flavour [0] = GameObject.Find ("SkeleFlav");
+		flavour [1] = GameObject.Find ("BruteFlav");
+		flavour [2] = GameObject.Find ("BansheeFlav");
+		flavour [0].SetActive (false);
+		flavour [1].SetActive (false);
+		flavour [2].SetActive (false);
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 		if (Input.GetKeyDown(KeyCode.Return) && GameMaster.currentPlayerSkeleton && !flavourIsActive){
-			flavour[0].SetActive (true);
+			flavour [0].SetActive (true);
 			flavourIsActive = true;
-		} else if (Input.GetKeyDown(KeyCode.Return) && flavourIsActive){
-			flavour[0].SetActive (false);
+		} else if (Input.GetKeyDown(KeyCode.Return) && GameMaster.currentPlayerBrute && !flavourIsActive){
+			flavour [1].SetActive (true);
+			flavourIsActive = true;
+		} else if (Input.GetKeyDown(KeyCode.Return) && GameMaster.currentPlayerBanshee && !flavourIsActive){
+			flavour [2].SetActive (true);
+			flavourIsActive = true;
+		}else if (Input.GetKeyDown(KeyCode.Return) && flavourIsActive){
+			for (int i=0; i<3; i++){
+				flavour [i].SetActive (false);
+			}
 			flavourIsActive = false;
 		}
 
