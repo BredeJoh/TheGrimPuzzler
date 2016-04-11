88d5124fe52648ef3016b661c238a1514c4c99d9
MartinLien <m.lsk95@hotmail.com>
2016-04-11 21:08:42 +0200

    enemy anim

---
+++ Assets/Scripts/enemySkeleAnim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class enemySkeleAnim : MonoBehaviour {
+
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = gameObject.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.GetComponent<Rigidbody2D> ().velocity != null) {
+			anim.SetInteger ("animationstate", 0);
+		} else {
+			anim.SetInteger ("animationstate", 1);
+		}
+	}
+}
