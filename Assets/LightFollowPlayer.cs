a1269a8e7f1bd349ba283698794a8879f197da40
BredeJoh <brede_96@hotmail.com>
2016-05-25 14:23:05 +0200

    lighting

---
+++ Assets/LightFollowPlayer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFollowPlayer : MonoBehaviour {
+
+    GameObject Player;
+
+	// Use this for initialization
+	void Start () {
+	
+        Player = GameObject.FindGameObjectWithTag("Player");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        gameObject.transform.position = Player.transform.position + new Vector3(0f, 0f, -6f);
+
+	}
+}
