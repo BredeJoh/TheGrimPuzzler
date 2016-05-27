e1ae3ff15de6d97b39d934093369fb823614bf8e
BredeJoh <brede_96@hotmail.com>
2016-05-27 13:30:06 +0200

    lighting++

--- Assets/LightFollowPlayer.cs
+++ Assets/LightFollowPlayer.cs
@@ -3,19 +3,19 @@ using System.Collections;
 
 public class LightFollowPlayer : MonoBehaviour {
 
-    GameObject Player;
+    GameObject camera;
 
 	// Use this for initialization
 	void Start () {
 	
-        Player = GameObject.FindGameObjectWithTag("Player");
+        camera = GameObject.Find("Main Camera");
 
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        gameObject.transform.position = Player.transform.position + new Vector3(0f, 0f, -4f);
+        gameObject.transform.position = camera.transform.position + new Vector3(0f, 0f, 6f);
 
 	}
 }
