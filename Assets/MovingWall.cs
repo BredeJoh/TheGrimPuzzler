0817f60cbfbeb7811adcfac8f4a148220bd6ba46
BredeJoh <brede_96@hotmail.com>
2016-05-29 20:45:12 +0200

    more level 4

--- Assets/MovingWall.cs
+++ Assets/MovingWall.cs
@@ -3,15 +3,26 @@ using System.Collections;
 
 public class MovingWall : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
+    public float speedUp = 0f;
+    public float speedSideWays = 0.03f;
+
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        gameObject.transform.position += new Vector3(0.02f, 0f, 0f);
+        gameObject.transform.position += new Vector3 (speedSideWays ,speedUp, 0f);
 
 	}
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            speedUp = -0.04f;
+        }
+    }
 }
